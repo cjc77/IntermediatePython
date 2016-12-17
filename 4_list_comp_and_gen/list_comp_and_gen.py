c4bd1d54e69e86fd88b4bf5c83479b897e3e0c49
carson <carsonjamescook@gmail.com>
2016-12-16 22:47:42 -0800

    Part 4.

---
+++ 4_list_comp_and_gen/list_comp_and_gen.py
@@ -0,0 +1,10 @@
+#! usr/bin/env python3
+
+# List Comprehension
+list_comp = [i for i in range(1, 11)]
+
+# Generator Obj
+gen_obj = (i for i in range(1, 11))
+
+print("List comp: ", list_comp, "\nGenerator obj: ", gen_obj, sep='')
+
