985969577cda9a5a1cf6324d9bd66c2b470cad42
carson <carsonjamescook@gmail.com>
2016-12-16 22:26:58 -0800

    Tutorial 2 done.

---
+++ 3_argparse/cli_demo.py
@@ -0,0 +1,29 @@
+#! usr/bin/env python3
+
+import argparse
+import sys
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("x", type=float, default=1.0,
+                        help="What is the first number?")
+    parser.add_argument("y", type=float, default=1.0,
+                        help="What is the second number?")
+    parser.add_argument("operation", type=str, default="add",
+                        help="What is the operation? (add, sub, mul, div")
+    args = parser.parse_args()
+    sys.stdout.write(str(calc(args)) + '\n')
+
+def calc(args):
+    if args.operation == "add":
+        return args.x + args.y
+    elif args.operation == "sub":
+        return args.x - args.y
+    elif args.operation == "mul":
+        return args.x * args.y
+    elif args.operation == "div":
+        return args.x / args.y
+
+if __name__ == "__main__":
+    main()
